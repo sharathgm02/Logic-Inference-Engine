24f96f6b168d4fe99b587bc2e22474b11733f246
Sharath Gowdar Mahendranath <smahendr@usc.edu>
2016-05-04 18:30:04 -0700

    Submitted Code

--- hw2cs561s16.py
+++ hw2cs561s16.py
@@ -1,271 +1,331 @@
-__author__ = 'Sharath_GM'
+import re
 import copy
+import sys
 
-VISITED_DICT = {}
-
-class Predicate:
-
-    def __init__(self, name=None, args=None):
-        if name is None and args is None:
-            self.name = ""
-            self.numArgs = 0
-            self.argArr = []
-
-        elif args is None:
-            self.name = name.name
-            self.numArgs = name.numArgs
-            self.argArr = name.argArr
-
-        else:
-            self.name = name
-            self.argArr = args.split(", ")
-            self.numArgs = len(self.argArr)
-            print self.name, "-----", self.argArr
-
-    def get_name(self):
-        return self.name
-
-    def get_arg_arr(self):
-        return self.argArr
-
-    def get_num_args(self):
-        return self.numArgs
-
-    def is_same(self, obj):
-        if obj is None or not isinstance(self, obj):
-            return False
-        term = Predicate(obj)
-        if not self.name == term.get_name():
-            return False
-        if not self.numArgs == term.get_num_args():
-            return False
-        for i in range(0, term.get_num_args()):
-            if is_arg_constant(self.get_arg_arr()[i]) or is_arg_constant(term.get_arg_arr()[i]):
-                if not self.get_arg_arr()[i] == term.get_arg_arr()[i]:
-                    return False
-        return True
-
-
-class ImplicationMapping:
-    def __init__(self, obj=None):
-        if obj is None:
-            self.consequent = Predicate()
-            self.antecedents = []
-        else:
-            self.consequent = obj.consquent
-            self.antecedents = obj.antecedents
-
-    def add_antecedent(self, sentence):
-        self.antecedents.append(sentence)
-
-    def is_mapping_same(self, obj):
-        if obj is None or not isinstance(self, obj):
-            return False
-        mapping_obj = ImplicationMapping(obj)
-        if not mapping_obj.consequent.is_same(self.consequent) or not mapping_obj.consequent.get_arg_arr() == self.consequent.get_arg_arr():
-            return False
-        if not len(mapping_obj.antecedents) == len(self.antecedents):
-            return False
-        for pred in mapping_obj.antecedents:
-            if pred not in self.antecedents:
-                return False
-            for another_pred in self.antecedents:
-                if pred.is_same(another_pred):
-                    if not pred.get_arg_arr() == another_pred.get_arg_arr():
-                        return False
-        return True
-
-
-class KnowledgeBase:
-
-    def __init__(self):
-        self.factsMap = {}
-        self.clausesMap = {}
-
-    def add_fact(self, name, loc_fact):
-        fmap = self.factsMap
-
-        if name in fmap:
-            if loc_fact not in fmap.get(name):
-                fmap.get(name).append(loc_fact)
-        else:
-            sentences = []
-            sentences.append(loc_fact)
-            fmap[name] = sentences
-
-    def add_clause(self, name, mapping_to_add):
-        cmap = self.clausesMap
-
-        if name in cmap:
-            sentences = cmap.get(name)
-            flag = False
-            for kb_term in sentences:
-                if kb_term.is_mapping_same(mapping_to_add):
-                    flag = True
-                    break
-            if not flag:
-                sentences.append(mapping_to_add)
-        else:
-            sentences = []
-            sentences.append(mapping_to_add)
-            cmap[name] = sentences
-
-    def print_kb(self):
-        for key in self.factsMap:
-            print key
-            print self.factsMap.get(key)
-            print "____________________"
-        for key in self.clausesMap:
-            print key
-            print self.clausesMap.get(key)
-            print "____________________"
+counter = 0
+visited_facts = []
+final_goal = None
+flag = False
+LOGGER = ""
+hack_print = False
+proved_facts_list = []
 
 
 def is_arg_constant(word):
     return word[0].isupper()
 
 
-def is_fact_unifiable(left, right):
-    left = Predicate(left)
-    right = Predicate(right)
-    match = False
-    args = right.get_arg_arr()
-    index = 0
-    for element in args:
-        match = False
-        if is_arg_constant(args[index]):
-            if args[index] == left.get_arg_arr()[index]:
-                match = True
-        else:
-            match = True
-        if not match:
-            break
-        index+=1
-    return match
+def is_variable(x):
+    return x == x.lower()
 
 
-def is_consequent_unifiable(consequent, goal):
-    goal = Predicate(goal)
-    consequent = Predicate(consequent)
-    for i in range(0, consequent.get_num_args()):
-        if is_arg_constant(consequent.get_arg_arr()[i]) and is_arg_constant(goal.get_arg_arr()[i]):
-            if not consequent.get_arg_arr()[i] == goal.get_arg_arr()[i]:
-                return False
+def append_all(var, val, theta):
+    new_theta = theta.copy()
+    new_theta[var] = val
+    return new_theta
+
+
+def unify_var(x, y, theta, goal=None):
+    global hack_print
+    # if bool(hack_print and goal is not None):
+    #     print_ask(goal)
+    #     hack_print = False
+    if x in theta:
+        return unify(theta[x], y, theta, goal)
+    elif y in theta:
+        return unify(x, theta[y], theta, goal)
+    else:
+        return append_all(x, y, theta)
+
+
+def is_list(x):
+    return hasattr(x, '__len__')
+
+
+def unify(x, y, theta={}, goal=None):
+
+    global hack_print
+    if theta is not None:
+        for key in theta.keys():
+            if theta.get(key) in theta.keys():
+                theta[key] = theta.get(theta.get(key))
+
+    if theta is None:
+        return None
+    elif x == y:
+        return theta
+
+    elif isinstance(x, str) and is_variable(x):
+        return unify_var(x, y, theta, goal)
+    elif isinstance(y, str) and is_variable(y):
+        return unify_var(y, x, theta, goal)
+    elif isinstance(x, str) or isinstance(y, str) or not x or not y:
+        if bool(hack_print):
+            hack_print = False
+        return theta if x == y else None
+    elif is_list(x) and is_list(y) and len(x) == len(y):
+        return unify(x[1:], y[1:], unify(x[0], y[0], theta, goal), goal)
+    else:
+        return None
+
+
+class Predicate:
+    def __copy__(self):
+        return Predicate(self.name, self.arguments)
+
+    def __hash__(self):
+        return hash((self.name, tuple(self.arguments)))
+
+    def __eq__(self, other):
+        return self.name == other.name and self.arguments == other.arguments
+
+    def __init__(self, name, arguments):
+        self.name = name
+        self.arguments = arguments
+
+
+def create_predicate(x):
+    operand_name = ''.join(re.findall(r'(\W?\w+)\(',x))
+    arg_terms = ''.join(re.findall(r'\(([\w+]|[\w+, \w+]*)\)',x)).split(', ')
+    return Predicate(operand_name,arg_terms)
+
+
+def create_predicate_conjuncts(x):
+    return [create_predicate(y) for y in x.split(' && ')] if x else []
+
+
+def get_lhs(x):
+    if "=>" in x:
+        return x.split(" => ")[0]
+    else:
+        return None
+
+
+def get_rhs(x):
+    return x.split(" => ")[-1]
+
+
+def replace_in_list(theta, pred_args):
+    new_list = list(pred_args)
+    for var in theta:
+        if var in pred_args:
+            indices = [i for (i,j) in enumerate(pred_args) if j == var]
+            for k in indices:
+                new_list[k] = theta[var]
+    return new_list
+
+
+def standardize(lhs, rhs, theta=None):
+    global counter
+    if theta is None:
+        theta = {}
+    args = []
+    for i in lhs:
+        for j in i.arguments:
+            args.append(j)
+
+    args = args + rhs.arguments
+
+    for term in args:
+        if is_variable(term):
+            if term not in theta:
+                counter += 1
+                theta[term] = "v_%d" % counter
+
+    for i in lhs:
+        i.arguments = replace_in_list(theta, i.arguments)
+    rhs.arguments = replace_in_list(theta, rhs.arguments)
+
+    return lhs, rhs
+
+
+def is_fact(term):
+    for i in term.arguments:
+        if is_variable(i):
+            return False
     return True
 
 
-def is_visited(goal_term):
-    goal_term = Predicate(goal_term)
-    if goal_term.get_name() in VISITED_DICT.keys():
-        for term in VISITED_DICT.get(goal_term.get_name()):
-            term = Predicate(term)
-            if term.is_same(goal_term):
-                return True
-    return False
+def substitute(theta, term):
+    temp_term = copy.deepcopy(term)
+    for var in theta:
+        if var in term.arguments:
+            temp_term.arguments[temp_term.arguments.index(var)] = theta[var]
+    return temp_term
 
 
-def add_to_visited(goal_term):
-    goal_term = Predicate(goal_term)
-    if goal_term.get_name() in VISITED_DICT.keys():
-        VISITED_DICT.get(goal_term.get_name()).append(goal_term)
+def print_in_format(goal, dictionary):
+    global LOGGER
+    global proved_facts_list
+    my_str = "("
+    for statement in goal.arguments:
+        if len(my_str) > 1:
+            my_str += ", "
+        if is_arg_constant(statement):
+            my_str += statement
+        elif bool(dictionary) and dictionary.get(statement) is not None:
+            if is_variable(dictionary.get(statement)):
+                my_str += "_"
+            else:
+                my_str += dictionary.get(statement)
+        else:
+            my_str += "_"
+    my_str += ")"
+    print "True:", goal.name, my_str
+    LOGGER += "True: " + goal.name + my_str + "\n"
+    proved_facts_list.append(goal)
+
+
+def print_for_false(goal, dictionary):
+    global LOGGER
+    global proved_facts_list
+    if goal in proved_facts_list:
+        return
+    my_str = "("
+    for statement in goal.arguments:
+        if len(my_str) > 1:
+            my_str += ', '
+        if is_arg_constant(statement):
+            my_str += statement
+        elif bool(dictionary) and dictionary.get(statement) is not None:
+            if is_variable(dictionary.get(statement)):
+                my_str += "_"
+            else:
+                my_str += dictionary.get(statement)
+        else:
+            my_str += "_"
+    my_str += ")"
+    print "False:", goal.name, my_str
+    LOGGER += "False: " + goal.name + my_str + "\n"
+
+
+def print_ask(goal):
+    global LOGGER
+    my_str = "("
+    for statement in goal.arguments:
+        if len(my_str) > 1:
+                my_str += ', '
+        if is_arg_constant(statement):
+            my_str += statement
+        else:
+            my_str += '_'
+    my_str += ")"
+    print "Ask:", goal.name, my_str
+    LOGGER += "Ask: " + goal.name + my_str + "\n"
+
+
+def fol_bc_and(knowledge_base, goals, theta, and_visited_facts):
+    global hack_print
+
+    if theta is None:
+        #hack_print = True
+        return
+
+    elif len(goals) == 0:
+        yield theta
     else:
-        term_list = []
-        term_list.append(goal_term)
-        VISITED_DICT[goal_term.get_name()] = term_list
+        first = goals[0]
+        rest = goals[1:]
+
+        for theta_dash in fol_bc_or(knowledge_base, substitute(theta, first), and_visited_facts, theta):
+            for theta_double_dash in fol_bc_and(knowledge_base, rest, theta_dash, and_visited_facts):
+                yield theta_double_dash
 
 
-def bc_fol_or(goal):
-    global VISITED_DICT
-    goal = Predicate(goal)
-    print "Ask: ", goal
-    unified = []
-    if goal.get_name() in kb.factsMap:
-        facts = kb.factsMap.get(goal.get_name())
-        for lhs in facts:
-            if is_fact_unifiable(lhs, goal):
-                unified.append(lhs)
+def fol_bc_or(knowledge_base, goal, visited_facts, theta={}):
+    global flag
+    global LOGGER
+    global hack_print
+    if isinstance(goal, Predicate):
+        print_ask(goal)
 
-    if is_visited(goal):
-        if not len(unified) == 0:
-            print "truee"
-            return unified
-        print "falsee"
-        return None
+    for rule in knowledge_base:
 
-    local_visited_map = copy.deepcopy(VISITED_DICT)
-    add_to_visited(goal)
+        lhs = create_predicate_conjuncts(get_lhs(rule))
+        rhs = create_predicate(get_rhs(rule))
 
-    if not goal.get_name() in kb.clausesMap.keys():
-        if not len(unified) == 0:
-            print "truee"
-            return unified
-        print "falsee"
-        return None
+        if rhs.name == goal.name:
+            # if bool(hack_print):
+            #     print_ask(goal)
+            #     hack_print = False
+            if is_fact(rhs):
+                if is_fact(goal):
+                    if not rhs == goal:
+                        continue
+                if rhs not in visited_facts:
+                    visited_facts.append(rhs)
 
-    sentences = kb.clausesMap.get(goal.get_name())
-    for rule in sentences:
-        rule = ImplicationMapping(rule)
-        consequent = Predicate(rule.consequent)
-        if not is_consequent_unifiable(consequent, goal):
-            continue
-        mapping = ImplicationMapping()
-        mapping.consequent = consequent
-        for term in rule.antecedents:
-            mapping.add_antecedent(Predicate(term))
+            lhs, rhs = standardize(lhs, rhs)
 
-        and_result = bc_fol_and(mapping)
-        if and_result is not None:
-            unified.extend(and_result)
-        VISITED_DICT = copy.deepcopy(local_visited_map)
-        if not len(unified) == 0:
-            print "truee"
-            return unified
-        print "falsee"
-        return None
+            dicto = {}
+            dicto = unify(rhs.arguments, goal.arguments, theta, goal)
+            if dicto == {}:
+                hack_print = False
+            else:
+                if bool(hack_print and goal is not None):
+                    print_ask(goal)
+                    hack_print = False
+
+            for theta_dash in fol_bc_and(knowledge_base, lhs, unify(rhs.arguments, goal.arguments, theta, goal), visited_facts):
+                print_in_format(goal, theta_dash)
+                if not goal == final_goal:
+                    yield theta_dash
+                else:
+                    flag = True
+                    return
+
+    print_for_false(goal, theta)
+    hack_print = True
+    yield
 
 
-def bc_ask(query):
-        return bc_fol_or(query) is not None
+def fol_bc_ask(knowledge_base, query):
+    global flag
+    flag = False
+    query = create_predicate(query)
+    visited_facts.append(query)
+    return fol_bc_or(knowledge_base, query, visited_facts, {})
 
 
+def read_file_data(f):
+    query_str = str(f.readline().rstrip())
+    queries = query_str.split(" && ")
+    num_kb_facts = int(f.readline().rstrip('\r\n'))
+    knowledge_base = []
+    for i in range(num_kb_facts):
+        knowledge_base.append(str(f.readline().rstrip()))
+
+    return queries, knowledge_base
 
 
-def create_predicate(term_str):
-    open_index = term_str.index("(")
-    close_index = term_str.index(")")
-    arguments = term_str[open_index + 1:close_index]
-    name = term_str[0:open_index]
-    return Predicate(name, arguments)
+def main():
+    global visited_facts
+    global counter
+    global final_goal
+    global flag
+    global LOGGER
+    input_file_handler = open(str(sys.argv[2]), 'r')
+    output_file_handler = open("output.txt", 'w')
+    queries, knowledge_base = read_file_data(input_file_handler)
 
+    for query in queries:
+        try:
+            final_goal = create_predicate(query)
+            bc_length = len(list(fol_bc_ask(knowledge_base, query)))
+            if not bool(flag):
+                break
+        except RuntimeError:
+            break
 
-#################
-# MAIN FUNCTION #
-#################
+        visited_facts = []
+        counter = 0
+    if flag is True:
+        LOGGER += "True"
+    else:
+        LOGGER += "False"
+    output_file_handler.write(LOGGER)
+    output_file_handler.close()
+    input_file_handler.close()
 
-f = open('sample01.txt', 'r')
-
-given_query = f.readline().rstrip()
-
-num_kb_clauses = int(f.readline().rstrip())
-kb = KnowledgeBase()
-
-for i in range(num_kb_clauses):
-    clause = f.readline().rstrip()
-    terms = clause.split("=>")
-
-    if len(terms) == 1:
-        fact = create_predicate(terms[0])
-        kb.add_fact(fact.get_name(), fact)
-
-    if len(terms) == 2:
-        consequent_term = create_predicate(terms[1])
-        conjuncts = terms[0].split("&&")
-        impl_mapping = ImplicationMapping()
-        impl_mapping.consequent = consequent_term
-        for statement in conjuncts:
-            impl_mapping.add_antecedent(create_predicate(statement))
-        kb.add_clause(consequent_term.get_name(), impl_mapping)
-
-kb.print_kb()
-
-final_result = bc_ask(given_query)
+if __name__ == '__main__':
+    main()
